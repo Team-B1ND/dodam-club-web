d43a021efb1741ea3240863f5487226c6a08a5cd
박재민 <96679652+Cartridge9@users.noreply.github.com>
2025-03-07 00:18:14 +0900

    chore :: cookie lib change #2
    Co-authored-by: 박상현 <dev.sanghyun9467@gmail.com>

--- src/libs/cookie/cookie.ts
+++ src/libs/cookie/cookie.ts
@@ -2,11 +2,7 @@ import cookie from "js-cookie";
 
 class Cookie {
   public getCookie(key: string): string | undefined {
-    let item = undefined;
-    if (cookie.get(key) !== undefined) {
-      item = cookie.get(key);
-    }
-    return item;
+return cookie.get(key) ?? undefined;
   }
 
   public setCookie(key: string, value: string): void {
