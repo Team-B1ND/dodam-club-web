e123801dd7e58ad78fabee19d7885a54e6466834
Cartridge0923 <pjmin0923@gmail.com>
2026-03-10 13:59:28 +0900

    fix: club list view with applicant start

--- src/components/ClubMain/index.tsx
+++ src/components/ClubMain/index.tsx
@@ -1,6 +1,7 @@
 import ClubList from "src/components/ClubMain/ClubList";
 import * as S from "./style";
 import { useClubTime } from "src/hooks/club/useClubTime";
+import dayjs from "dayjs";
 
 
 const ClubMain = () => {
@@ -11,7 +12,7 @@ const ClubMain = () => {
       <S.ClubListHead>동아리</S.ClubListHead>
       {timeIsLoading ? (
         <>loading....</>
-      ) : timeData!.createEnd >= today ? (
+      ) : dayjs(today).isBefore(timeData!.applicantStart) ? (
         <S.ClubMessage>
           현재 동아리 개설 기간입니다.
         </S.ClubMessage>
