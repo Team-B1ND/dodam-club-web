147c0456959074bb66cfdc799004bad6d6ece7e1
Cartridge0923 <pjmin0923@gmail.com>
2026-03-11 10:01:07 +0900

    fix: remove club item state

--- src/components/ClubMain/ClubList/ClubItem/index.tsx
+++ src/components/ClubMain/ClubList/ClubItem/index.tsx
@@ -1,12 +1,10 @@
 import * as S from './style'
-import { Clock, DodamColor, XmarkCircle } from '@b1nd/dds-web'
 import { ClubProps } from 'src/types/club/club.type'
-import { EClubState } from 'src/enum/club/club.enum'
 
 
 
 const ClubItem = ({ value } : ClubProps) => {
-  const { name, shortDescription, state, image } = value
+  const { name, shortDescription, image } = value
   return (
     <S.ClubItem>
       <S.ClubItemImage src={image}/>
@@ -15,7 +13,6 @@ const ClubItem = ({ value } : ClubProps) => {
           <S.ClubItemName>
             {name}
           </S.ClubItemName>
-          {(state === EClubState.PENDING || state === EClubState.WAITING) && <Clock color={DodamColor.yellow50} size={24}/>}
         </S.ClubItemNameState>
         <S.ClubItemDescription>{shortDescription}</S.ClubItemDescription>
       </S.ClubItemInfoContainer>
