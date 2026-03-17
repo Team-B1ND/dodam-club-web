314378d9498d453367bad10a252386e0e51b7e28
Jaemin Park <96679652+Cartridge9@users.noreply.github.com>
2026-03-17 13:43:53 +0900

    hotfix: logic

--- src/components/ClubMenuBar/ClubMenu/StudentApplyMenu/index.tsx
+++ src/components/ClubMenuBar/ClubMenu/StudentApplyMenu/index.tsx
@@ -5,53 +5,53 @@ import { DodamFilledButton } from "@b1nd/dds-web";
 import { useNavigate } from "react-router-dom";
 
 interface StudentApplyMenuProps {
-  time: string;
+    time: string;
 }
 const StudentApplyMenu = ({
-  time
+    time
 }: StudentApplyMenuProps) => {
-  const { data } =
-    useGetStudentApplyQuery();
-  const navigate = useNavigate();
+    const { data } =
+        useGetStudentApplyQuery();
+    const navigate = useNavigate();
 
-  return data!.length > 0 ? (
-   {data!.filter(item => item.status === "ALLOWED").length === 0 ? (
-    <DodamFilledButton
-      size={"Large"}
-      text="동아리 입부 신청하기"
-      textTheme="staticWhite"
-      typography={["Body2", "Bold"]} onClick={() => navigate("/register")}
-    />
-   ) : ()}
-    <S.MenuItemContainer>
-      <p>내 동아리 정보</p>
-      {data?.map((item) => (
-        <div>
-          <p>{item.club.name}</p>
-          <StudentApplyStatusText status={item.status} />
-        </div>
-      ))}
-    </S.MenuItemContainer>
-  ) : (
+    return data!.length > 0 ? (
+        <>
+            <DodamFilledButton
+                size={"Large"}
+                text="동아리 입부 신청하기"
+                textTheme="staticWhite"
+                typography={["Body2", "Bold"]} onClick={() => navigate("/register")}
+            />
+            <S.MenuItemContainer>
+                <p>내 동아리 정보</p>
+                {data?.map((item) => (
+                    <div>
+                        <p>{item.club.name}</p>
+                        <StudentApplyStatusText status={item.status} />
+                    </div>
+                ))}
+            </S.MenuItemContainer>
+        </>
+    ) : (
 
-    <>
-      <DodamFilledButton
-        size={"Large"}
-        text="동아리 입부 신청하기"
-        textTheme="staticWhite"
-        typography={["Body2", "Bold"]}
-        onClick={() => navigate("/register")}
-      />
-      <S.MyClubIsNone>
-        <p>
-          아직 동아리에 <br /> 신청하지 않았어요!
-        </p>
-        <div>
-          <span>신청 마감 : {time}</span>
-        </div>
-      </S.MyClubIsNone>
-    </>
-  );
+        <>
+            <DodamFilledButton
+                size={"Large"}
+                text="동아리 입부 신청하기"
+                textTheme="staticWhite"
+                typography={["Body2", "Bold"]}
+                onClick={() => navigate("/register")}
+            />
+            <S.MyClubIsNone>
+                <p>
+                    아직 동아리에 <br /> 신청하지 않았어요!
+                </p>
+                <div>
+                    <span>신청 마감 : {time}</span>
+                </div>
+            </S.MyClubIsNone>
+        </>
+    );
 }
 
 export default StudentApplyMenu;
